3d9b896bd896e76714c933e0c7daa9098d568a2c
Bennu-Li <yunmei.li@zilliz.com>
2022-07-28 16:58:06 +0800

    Add feature for cp and get
    Signed-off-by: Bennu-Li <yunmei.li@zilliz.com>

---
+++ internal/cmd/get/get.go
@@ -0,0 +1,136 @@
+package get
+
+import (
+	// "bytes"
+	// "context"
+	// "encoding/json"
+	"fmt"
+	// "io"
+	// "io/ioutil"
+	// "net/url"
+	// "os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	// corev1 "k8s.io/api/core/v1"
+	// apierrors "k8s.io/apimachinery/pkg/api/errors"
+	// "k8s.io/apimachinery/pkg/api/meta"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	// metav1beta1 "k8s.io/apimachinery/pkg/apis/meta/v1beta1"
+	// "k8s.io/apimachinery/pkg/runtime"
+	// utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	// "k8s.io/apimachinery/pkg/util/sets"
+	// "k8s.io/apimachinery/pkg/watch"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	// "k8s.io/cli-runtime/pkg/printers"
+	// "k8s.io/cli-runtime/pkg/resource"
+	// kubernetesscheme "k8s.io/client-go/kubernetes/scheme"
+	// "k8s.io/client-go/rest"
+	// watchtools "k8s.io/client-go/tools/watch"
+	"k8s.io/kubectl/pkg/cmd/apiresources"
+	kubectlget "k8s.io/kubectl/pkg/cmd/get"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	// "k8s.io/kubectl/pkg/rawhttp"
+	// "k8s.io/kubectl/pkg/scheme"
+	"k8s.io/kubectl/pkg/util/i18n"
+	// "k8s.io/kubectl/pkg/util/interrupt"
+	"k8s.io/kubectl/pkg/util/templates"
+	// utilpointer "k8s.io/utils/pointer"
+)
+
+var (
+	getLong = templates.LongDesc(i18n.T(`
+		Display one or many resources.
+		Prints a table of the most important information about the specified resources.
+		You can filter the list using a label selector and the --selector flag. If the
+		desired resource type is namespaced you will only see results in your current
+		namespace unless you pass --all-namespaces.
+		Uninitialized objects are not shown unless --include-uninitialized is passed.
+		By specifying the output as 'template' and providing a Go template as the value
+		of the --template flag, you can filter the attributes of the fetched resources.`))
+
+	getExample = templates.Examples(i18n.T(`
+		# List all pods in ps output format
+		kubectl get pods
+		# List all pods in ps output format with more information (such as node name)
+		kubectl get pods -o wide
+		# List a single replication controller with specified NAME in ps output format
+		kubectl get replicationcontroller web
+		# List deployments in JSON output format, in the "v1" version of the "apps" API group
+		kubectl get deployments.v1.apps -o json
+		# List a single pod in JSON output format
+		kubectl get -o json pod web-pod-13je7
+		# List a pod identified by type and name specified in "pod.yaml" in JSON output format
+		kubectl get -f pod.yaml -o json
+		# List resources from a directory with kustomization.yaml - e.g. dir/kustomization.yaml
+		kubectl get -k dir/
+		# Return only the phase value of the specified pod
+		kubectl get -o template pod/web-pod-13je7 --template={{.status.phase}}
+
+		# List resource information in custom columns
+		kubectl get pod test-pod -o custom-columns=CONTAINER:.spec.containers[0].name,IMAGE:.spec.containers[0].image
+		# List all replication controllers and services together in ps output format
+		kubectl get rc,services
+		# List one or more resources by their type and names
+		kubectl get rc/web service/frontend pods/web-pod-13je7`))
+)
+
+const (
+	useOpenAPIPrintColumnFlagLabel = "use-openapi-print-columns"
+	useServerPrintColumns          = "server-print"
+)
+
+func NewMilvusGetCmd(parent string, f cmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	o := kubectlget.NewGetOptions(parent, streams)
+
+	cmd := &cobra.Command{
+		Use:                   fmt.Sprintf("get [(-o|--output=)%s] (TYPE[.VERSION][.GROUP] [NAME | -l label] | TYPE[.VERSION][.GROUP]/NAME ...) [flags]", strings.Join(o.PrintFlags.AllowedFormats(), "|")),
+		DisableFlagsInUseLine: true,
+		Short:                 i18n.T("Display one or many resources"),
+		Long:                  getLong + "\n\n" + cmdutil.SuggestAPIResources(parent),
+		Example:               getExample,
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			var comps []string
+			if len(args) == 0 {
+				comps = apiresources.CompGetResourceList(f, cmd, toComplete)
+			} else if len(args) == 1 {
+				comps = kubectlget.CompGetResource(f, cmd, args[0], toComplete)
+			}
+			return comps, cobra.ShellCompDirectiveNoFileComp
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			cmdutil.CheckErr(o.Complete(f, cmd, args))
+			cmdutil.CheckErr(o.Validate(cmd))
+			cmdutil.CheckErr(o.Run(f, cmd, args))
+		},
+		SuggestFor: []string{"list", "ps"},
+	}
+
+	o.PrintFlags.AddFlags(cmd)
+
+	cmd.Flags().StringVar(&o.Raw, "raw", o.Raw, "Raw URI to request from the server.  Uses the transport specified by the kubeconfig file.")
+	cmd.Flags().BoolVarP(&o.Watch, "watch", "w", o.Watch, "After listing/getting the requested object, watch for changes. Uninitialized objects are excluded if no object name is provided.")
+	cmd.Flags().BoolVar(&o.WatchOnly, "watch-only", o.WatchOnly, "Watch for changes to the requested object(s), without listing/getting first.")
+	cmd.Flags().BoolVar(&o.OutputWatchEvents, "output-watch-events", o.OutputWatchEvents, "Output watch event objects when --watch or --watch-only is used. Existing objects are output as initial ADDED events.")
+	cmd.Flags().BoolVar(&o.IgnoreNotFound, "ignore-not-found", o.IgnoreNotFound, "If the requested object does not exist the command will return exit code 0.")
+	cmd.Flags().StringVarP(&o.LabelSelector, "selector", "l", o.LabelSelector, "Selector (label query) to filter on, supports '=', '==', and '!='.(e.g. -l key1=value1,key2=value2)")
+	cmd.Flags().StringVar(&o.FieldSelector, "field-selector", o.FieldSelector, "Selector (field query) to filter on, supports '=', '==', and '!='.(e.g. --field-selector key1=value1,key2=value2). The server only supports a limited number of field queries per type.")
+	cmd.Flags().BoolVarP(&o.AllNamespaces, "all-namespaces", "A", o.AllNamespaces, "If present, list the requested object(s) across all namespaces. Namespace in current context is ignored even if specified with --namespace.")
+	addOpenAPIPrintColumnFlags(cmd, o)
+	addServerPrintColumnFlags(cmd, o)
+	cmdutil.AddFilenameOptionFlags(cmd, &o.FilenameOptions, "identifying the resource to get from a server.")
+	cmdutil.AddChunkSizeFlag(cmd, &o.ChunkSize)
+
+	return cmd
+}
+
+func addOpenAPIPrintColumnFlags(cmd *cobra.Command, opt *kubectlget.GetOptions) {
+	cmd.Flags().BoolVar(&opt.PrintWithOpenAPICols, useOpenAPIPrintColumnFlagLabel, opt.PrintWithOpenAPICols, "If true, use x-kubernetes-print-column metadata (if present) from the OpenAPI schema for displaying a resource.")
+	cmd.Flags().MarkDeprecated(useOpenAPIPrintColumnFlagLabel, "deprecated in favor of server-side printing")
+}
+
+func addServerPrintColumnFlags(cmd *cobra.Command, opt *kubectlget.GetOptions) {
+	cmd.Flags().BoolVar(&opt.ServerPrint, useServerPrintColumns, opt.ServerPrint, "If true, have the server return the appropriate table output. Supports extension APIs and CRDs.")
+}
